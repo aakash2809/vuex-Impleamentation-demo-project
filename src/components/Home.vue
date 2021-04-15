a0f298e69dbb2d74cc849f004cba2c44433a25b4
aakash2809 <aakashrajak289@gmail.com>
2021-04-15 12:36:59 +0530

    initial commit

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!--  <h1>{{ msg }}</h1>
+    <h2>{{ data }}</h2> -->
+    <button>call hello function</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "Home",
+  props: {
+    msg: String,
+    data: String,
+  },
+  methods: {
+    hello() {
+      console.log("");
+    },
+  },
+};
+</script>
